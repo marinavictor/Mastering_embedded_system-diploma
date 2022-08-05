e1b78ffc390ca759694750416f402ee48a8003df
marina <marinavictor280@gmail.com>
2022-08-05 11:56:24 +0200

    problem 2

---
+++ problem2.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : problem2.c
+ Author      : marina
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+int main()
+{
+int x;
+printf("Enter an integer: ");
+fflush(stdin);fflush(stdout);
+scanf("%d",&x);
+printf("you entered:%d ",x);
+return 0;
+}
+
