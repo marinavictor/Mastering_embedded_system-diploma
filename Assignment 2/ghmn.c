2854d90b37fa13dabab39a91bbe1f44aa3a49a37
marina <marinavictor280@gmail.com>
2023-02-21 17:49:46 +0200

    new trial

---
+++ Assignment 2/ghmn.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : ghmn.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include"stdio.h"
+#include"string.h"
+int main()
+{
+
+printf("hello");
+return 0;
+}
